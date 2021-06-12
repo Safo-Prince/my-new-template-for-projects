3fca37e8c63540814ec2e4c929146dedbdb1f731
Safo Prince <psam64850@gmail.com>
2021-06-12 22:48:13 +0000

    added twitter api and fuctionalities to our button

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
+const quoteContainer =document.getElementById('quote-container')
+const quoteText =document.getElementById('quote')
+const authorText =document.getElementById('author')
+const twitterBtn =document.getElementById('twitter')
+const newQuoteBtn =document.getElementById('new-quote')
+
+
 async function getQuote(){
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
 const apiURL ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+
 try{
 const response = await fetch(proxyUrl + apiURL);
 const data = await response.json()
+// validating our author name because some quotes don't have authors
+if(data.quoteAuthor ===''){
+    authorText.innerText = 'unknown'
+}else{
+    authorText.innerText =data.quoteAuthor;
+}
+
+// reducing the font size for quotes with long words
+if(data.quoteText.lenght > 120){
+    quoteText.classList.add('long-quote');
+}else{
+    quoteText.classList.remove('long-quote');
+}
+quoteText.innerText = data.quoteText;
 }catch(error){
-    console.log('whoops,no quote',error)
+    
 }
 }
+
+function tweetQuote(){
+    const quote = quoteText.innerText ;
+    const author = authorText.innerText;
+    const twitterUrl =`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank')
+  }
+
+  // Event listener
+newQuoteBtn.addEventListener('click',getQuote)
+twitterBtn.addEventListener('click',tweetQuote)
 getQuote()
